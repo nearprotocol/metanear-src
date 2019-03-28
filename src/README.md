c20f33f4b2383ddc8983d1ca179cfd49b3936397
Illia Polosukhin <ilblackdragon@gmail.com>
2019-03-28 13:13:18 -0700

    Add next.js code to build REACT + JSX

---
+++ src/README.md
@@ -0,0 +1,13 @@
+
+Install
+
+    npm install
+
+Local dev:
+
+    npm run dev
+
+Export:
+    
+    npm run build
+    npm rin export
---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+import Link from 'next/link'
+
+export default function Index() {
+    return (
+        <div>
+            <h1>My Blog</h1>
+        </div>
+    )
+}
