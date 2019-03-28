3fb0721fb4a56d45081017d8afbab962a2f44e45
Illia Polosukhin <ilblackdragon@gmail.com>
2019-03-28 15:00:32 -0700

    Add login and player movement

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,13 +3,16 @@ import Link from 'next/link'
 import Head from 'next/head'
 // import { Near } from 'nearlib'
 
-const accountId = "alice.near";
-const contractName = "metanear";
+const contractId = "metanear";
+const appTitle = "Meta NEAR"
+const baseUrl = "http://localhost:3000"
 
 class Grid extends React.Component {
-    componentDidMount() {
+    componentDidUpdate(prevProps, prevState, snapshot) {
         const canvas = this.refs.canvas
         const ctx = canvas.getContext("2d")
+        ctx.clearRect(0, 0, this.props.width, this.props.height);
+        ctx.fillStyle = "#FF0000";
         for (let i = 0; i < this.props.width / this.props.cellWidth; ++i) {
             ctx.beginPath()
             ctx.moveTo(i * this.props.cellWidth, 0)
@@ -42,7 +45,30 @@ class Grid extends React.Component {
     }
     render() {
         return (
-            <canvas ref="canvas" width={this.props.width} height={this.props.height} />
+            <canvas ref="canvas" width={this.props.width} height={this.props.height} onClick={this.props.onClick} />
+        )
+    }
+}
+
+class WalletLogin extends React.Component {
+    render() {
+        return (
+            <div id="sign-in-container">
+                <h2> Hello stranger! Who are you?</h2>
+                <div id="login-form">
+                    <div className="col-md-4"><button onClick={this.props.onClick} id="login-button" className="btn btn-lg btn-block btn-primary">Login with NEAR Wallet</button></div>
+                </div>
+            </div> 
+        )
+    }
+}
+
+class WalletLogout extends React.Component {
+    render() {
+        return (
+            <div id="hello">
+                <h2 id="hello">Hi, {this.props.accountId}!</h2>
+            </div>
         )
     }
 }
@@ -51,29 +77,33 @@ class Game extends React.Component {
     constructor(props) {
         super(props)
         this.state = { 
-            cells: [{ location: { x: 1, y: 1 }, viewIndex: 1, contractId: "ads", webUrl: "123", owner: "xyz" }], 
+            cells: [], 
             highlighCell: {},
-            player: {location: {x: 0, y: 0}}
+            player: null
         }
+        this.walletAccount = null
     }
     nearConnect = async () => {
-        const walletAccount = new nearlib.WalletAccount(contractName, "https://wallet.nearprotocol.com/");
-        const accountId = walletAccount.getAccountId();
+        this.walletAccount = new nearlib.WalletAccount(contractId, "https://wallet.nearprotocol.com/");
+        const accountId = this.walletAccount.getAccountId();
         console.log(accountId)
         const near = new nearlib.Near(new nearlib.NearClient(
-            walletAccount,
+            this.walletAccount,
             new nearlib.LocalNodeConnection("https://studio.nearprotocol.com/devnet"),
         ));
-        this.contract = await near.loadContract(contractName, {
+        this.contract = await near.loadContract(contractId, {
             viewMethods: ["lookAround", "getPlayer"],
-            changeMethods: [],
+            changeMethods: ["move"],
             sender: accountId,
         });
-        let view = await this.contract.lookAround()
+        let view = await this.contract.lookAround({ accountId })
         console.log(view)
-        let player = await this.contract.getPlayer({accountId: this.props.accountId})
-        console.log(player)
-        this.setState({cells: view.cells || [], player})
+        this.setState({ cells: view.cells || [] })
+        if (accountId) {
+            let player = await this.contract.getPlayer({ accountId })
+            console.log(player)
+            this.setState({player})
+        }
     }
     componentDidMount() {
         this.nearConnect();
@@ -88,11 +118,54 @@ class Game extends React.Component {
         }
         this.setState({highlighCell})
     }
+    login = () => {
+        this.walletAccount.requestSignIn(
+            contractId,
+            appTitle,
+            baseUrl + '/',
+            baseUrl + '/',
+        );
+    }
+    logout = () => {
+
+    }
+    movePlayer = () => {
+        let dx = 0
+        let dy = 0
+        if (Math.abs(this.state.player.location.x - this.state.highlighCell.location.x) == 1) {
+            dx = this.state.player.location.x > this.state.highlighCell.location.x ? -1 : 1
+        } else if (Math.abs(this.state.player.location.y - this.state.highlighCell.location.y) == 1) {
+            dy = this.state.player.location.y > this.state.highlighCell.location.y ? -1 : 1
+        }
+        this.contract.move({dx, dy}).then(() => {
+            const accountId = this.walletAccount.getAccountId();
+            this.contract.getPlayer({ accountId }).then(async (player) => {
+                console.log(player)
+                this.setState({ player })
+                let view = await this.contract.lookAround({ accountId })
+                console.log(view)
+                this.setState({ cells: view.cells || [] })
+            })
+        })
+        // console.log(this.state.highlighCell)
+    }
     render() {
+        let control;
+        if (this.walletAccount && this.walletAccount.isSignedIn()) {
+            control = <WalletLogout accountId={this.walletAccount.getAccountId()} onClick={this.logout} />
+        } else {
+            control = <WalletLogin onClick={this.login} />
+        }
         return (
             <div>
-                <Grid width={640} height={425} cellWidth={20} cellHeight={20} cells={this.state.cells} onHighlight={this.onHighlight} playerX={this.state.player.location.x} playerY={this.state.player.location.y} />
-                <div>{JSON.stringify(this.state.highlighCell)}</div>
+                {control}
+                <Grid width={640} height={425} cellWidth={20} cellHeight={20} 
+                cells={this.state.cells} onHighlight={this.onHighlight} 
+                    playerX={this.state.player && this.state.player.location.x} 
+                    playerY={this.state.player && this.state.player.location.y} 
+                onClick={this.movePlayer} />
+                <div>Highlighted cell: {JSON.stringify(this.state.highlighCell)}</div>
+                <div>Player: {JSON.stringify(this.state.player)}</div>
             </div>
         )
     }
@@ -105,7 +178,7 @@ export default function Index() {
                 <script src="https://cdn.jsdelivr.net/npm/nearlib@0.4.2/dist/nearlib.js"></script>
             </Head>
             <div>
-                <Game accountId="illia.near" />
+                <Game />
             </div>
         </div>
     )
