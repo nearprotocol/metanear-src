24b94ae8a0b80d0fbe71d985709c242ebc1275c6
Evgeny Kuzyakov <h3r0k1ll3r@gmail.com>
2019-03-28 11:48:32 -0700

    Point to devnet

--- docs/settings.js
+++ docs/settings.js
@@ -2,5 +2,6 @@ const settings = {
   deps: {
     createAccount: nearlib.dev.createAccountWithLocalNodeConnection
   },
+  nodeUrl = 'https://studio.nearprotocol.com/devnet',
 };
 const contractName = "metanear"; /* TODO: fill this in! */
